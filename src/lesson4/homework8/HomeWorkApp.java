89c4575847f05d0ed7d544ba869beae893275bd0
vhumenny <v.humenny@gmail.com>
2022-10-16 23:23:53 +0300

    hw8 almost done

--- src/lesson4/homework8/HomeWorkApp.java
+++ src/lesson4/homework8/HomeWorkApp.java
@@ -3,7 +3,7 @@ package lesson4.homework8;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class HomeWorkApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
@@ -48,69 +48,50 @@ public class Main {
 
     public static String stringReverse(String r) {
         char[] array = r.toCharArray();
-        StringBuilder reversed = new StringBuilder();
+        String reversed = "";
 
         for (int i = array.length - 1; i >= 0; i--) {
-            reversed.append(array[i]);
+            reversed += array[i];
         }
-        return reversed.toString();
+        return reversed;
     }
 
     public static boolean isPalindrome(String p) {
-        StringBuilder reversed = new StringBuilder();
+        String reversed = "";
 
         for (int i = 0; i < p.length(); i++) {
-            reversed.append(p.charAt(i));
+            reversed += p.charAt(i);
         }
-        return p.intern() == reversed.toString().intern();
+        return p.intern() == reversed.intern();
     }
 
     public static void guessTheWord(Scanner scanner, Random random) {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
                 "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
                 "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        System.out.println(words);
         int numberOfHiddenWord = random.nextInt(0, words.length - 1);
         String hiddenWord = words[numberOfHiddenWord];
-        String usersWord=scanner.next();
-        StringBuilder stringToShow = new StringBuilder();
+        System.out.println("Please enter the hidden word: ");
+        String usersWord = scanner.next();
+
 
         while (hiddenWord.intern() != usersWord.intern()) {
-            for (int i = 0; i <= hiddenWord.length(); i++) {
-                if (hiddenWord.charAt(i) == usersWord.charAt(i)) stringToShow.append(usersWord.charAt(i));
-                else stringToShow.append("#");
-            }
-            if (stringToShow.length() < 16) stringToShow.append("#");
-            System.out.println(stringToShow);
 
+            char[] arrayOfChars = new char[15];
+
+            for (int i = 0; i < hiddenWord.length(); i++) {
+                if (hiddenWord.charAt(i) == usersWord.charAt(i)) arrayOfChars[i] = usersWord.charAt(i);
+                else arrayOfChars[i] = '#';
+            }
+            for (int i = hiddenWord.length(); i < arrayOfChars.length; i++) {
+                arrayOfChars[i] = '#';
+            }
+            String stringToShow = String.copyValueOf(arrayOfChars);
+            System.out.println("prompt: " + stringToShow);
             System.out.println("Please try again and guess the hidden word. Enter your word :");
             usersWord = scanner.next();
-
-
         }
-        if (hiddenWord.intern() == usersWord.intern())
-            System.out.println("Congratulations, you have guessed the hidden word - " + hiddenWord);
+        System.out.println("Congratulations, you have guessed the hidden word - " + hiddenWord);
     }
 }
-//    contains, startWith, valueOf
-
-//    public boolean startsWith(String prefix)
-//Проверяет, начинается ли строка символами prefix с начала строки
-//
-//
-//String str1 = "котлета";
-//
-//if(str1.startsWith("кот"))
-//    infoTextView.setText("Слово содержит кота");
-//else
-//    infoTextView.setText("Плохое слово. Нет смысла его использовать");
-
-
-//    public boolean contains (CharSequence cs)
-//Определяет, содержит ли строка последовательность символов в CharSequence
-//
-//
-//String testString = "котёнок";
-//
-//if(testString.contains("кот")){
-//    infoTextView.setText("В слове котёнок содержится слово кот!");
-//}
