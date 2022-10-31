a36a8cfa2b7407e0eb0389b085fe7370f482735c
vhumenny <v.humenny@gmail.com>
2022-10-31 20:31:14 +0200

    hw11 corrected

--- src/lesson8/homework11/Main.java
+++ src/lesson8/homework11/Main.java
@@ -1,7 +1,7 @@
 package lesson8.homework11;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
         ValueCalculator valueCalculator = new ValueCalculator();
 
--- src/lesson8/homework11/ValueCalculator.java
+++ src/lesson8/homework11/ValueCalculator.java
@@ -4,39 +4,52 @@ public class ValueCalculator {
     private static final int arrayLength = 1111000;
     private float[] array = new float[arrayLength];
     private int halfArrayLength = arrayLength / 2;
+    private float[] array1Part = new float[halfArrayLength];
+    private float[] array2Part = new float[halfArrayLength];
 
-    public long doCalc() {
+    public long doCalc() throws InterruptedException {
         long start = System.currentTimeMillis();
         float numForArray = 1;
 
         for (int i = 0; i < arrayLength; i++) {
             array[i] = numForArray;
         }
-        float[] array1 = new float[halfArrayLength];
-        float[] array2 = new float[halfArrayLength];
-        try {
-            System.arraycopy(array, 0, array1, 0, halfArrayLength);
-            System.arraycopy(array, halfArrayLength, array2, 0, halfArrayLength);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        arrayFiller(array1);
-        arrayFiller(array2);
-        try {
-            System.arraycopy(array1, 0, array, 0, halfArrayLength);
-            System.arraycopy(array2, 0, array, halfArrayLength, halfArrayLength);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        array1Part = new float[halfArrayLength];
+        array2Part = new float[halfArrayLength];
+
+        System.arraycopy(array, 0, array1Part, 0, halfArrayLength);
+        System.arraycopy(array, halfArrayLength, array2Part, 0, halfArrayLength);
+
+        Thread thread1 = new Thread(new Thread1());
+        Thread thread2 = new Thread(new Thread2());
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        System.arraycopy(array1Part, 0, array, 0, halfArrayLength);
+        System.arraycopy(array2Part, 0, array, halfArrayLength, halfArrayLength);
+
         return System.currentTimeMillis() - start;
     }
 
-    public void arrayFiller(float[] newArray) {
-        new Thread(() -> {
-            for (int i = 0; i < halfArrayLength; i++) {
-                newArray[i] = (float) (newArray[i] * Math.sin(0.2f + i / 5) *
+    public class Thread1 implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < array1Part.length; i++) {
+                array1Part[i] = (float) (array1Part[i] * Math.sin(0.2f + i / 5) *
                         Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-        }).start();
+        }
+    }
+
+    public class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < array2Part.length; i++) {
+                array2Part[i] = (float) (array2Part[i] * Math.sin(0.2f + i / 5) *
+                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }
     }
 }
