351cf8ed4dbbc8c26a6937b222d320edee3a7e50
vhumenny <v.humenny@gmail.com>
2022-10-31 17:51:14 +0200

    hw11 corrected

--- src/lesson8/homework11/ValueCalculator.java
+++ src/lesson8/homework11/ValueCalculator.java
@@ -16,32 +16,27 @@ public class ValueCalculator {
         float[] array2 = new float[halfArrayLength];
         try {
             System.arraycopy(array, 0, array1, 0, halfArrayLength);
-            System.arraycopy(array, 0, array2, 0, halfArrayLength);
+            System.arraycopy(array, halfArrayLength, array2, 0, halfArrayLength);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        new Thread(() -> {
-            for (int i = 0; i < halfArrayLength; i++) {
-
-                array1[i] = (float) (array1[i] * Math.sin(0.2f + i / 5) *
-                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        }).start();
-
-        new Thread(() -> {
-            for (int i = 0; i < halfArrayLength; i++) {
-
-                array2[i] = (float) (array2[i] * Math.sin(0.2f + i / 5) *
-                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        }).start();
-
+        arrayFiller(array1);
+        arrayFiller(array2);
         try {
             System.arraycopy(array1, 0, array, 0, halfArrayLength);
-            System.arraycopy(array2, 0, array, halfArrayLength - 1, halfArrayLength);
+            System.arraycopy(array2, 0, array, halfArrayLength, halfArrayLength);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return System.currentTimeMillis() - start;
+    }
+
+    public void arrayFiller(float[] newArray) {
+        new Thread(() -> {
+            for (int i = 0; i < halfArrayLength; i++) {
+                newArray[i] = (float) (newArray[i] * Math.sin(0.2f + i / 5) *
+                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }).start();
     }
 }
---
+++ src/lesson8/homework12/ThreadSafeList.java
@@ -0,0 +1,6 @@
+package lesson8.homework12;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+//public class ThreadSafeList implements CopyOnWriteArrayList {
+//}
