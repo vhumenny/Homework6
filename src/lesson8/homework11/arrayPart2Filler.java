a4cfd42963f5480e681bb0f090b9474b3425e093
vhumenny <v.humenny@gmail.com>
2022-10-31 13:18:03 +0200

    hw11 in process

--- src/lesson8/homework11/arrayPart2Filler.java
+++ src/lesson8/homework11/arrayPart2Filler.java
@@ -1,4 +1,14 @@
 package lesson8.homework11;
 
 public class arrayPart2Filler implements Runnable {
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < ValueCalculator.getHalfArrayLength(); i++) {
+
+            ValueCalculator.getArray()[i] = (float) (ValueCalculator.getArray()[i] * Math.sin(0.2f + i / 5) *
+                    Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
 }
